21f045877a6605e55b2cb2827afe6663aeac2441
Maewenn <mw.porgeon@gmail.com>
2025-04-13 17:32:33 +0200

    création arborescence API non exhaustive

--- backend/src/index.php
+++ backend/src/index.php
@@ -1,10 +1,10 @@
 <?php
-require_once __DIR__ . '/../database.php';
-require_once __DIR__ . '/../vendor/autoload.php';
+// require_once __DIR__ . '/../database.php';
+// require_once __DIR__ . '/../vendor/autoload.php';
 
-try {
-  $db = Database::getConnection();
-  echo "✅ Connexion à la base de données réussie !";
-} catch (Exception $e) {
-  echo "❌ Échec de la connexion : " . $e->getMessage();
-}
+// try {
+//   $db = Database::getConnection();
+//   echo "✅ Connexion à la base de données réussie !";
+// } catch (Exception $e) {
+//   echo "❌ Échec de la connexion : " . $e->getMessage();
+// }
